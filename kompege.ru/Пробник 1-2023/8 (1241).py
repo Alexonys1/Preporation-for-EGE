026012bb70045987eb0dfd4fc4b47308ae6cda25
Alex <leha.bondar.05@mail.ru>
2023-12-17 20:35:32 +0300

    Изменёно 8-е задание, чтобы выводился правильный ответ.

--- kompege.ru/Пробник 1-2023/8 (1241).py
+++ kompege.ru/Пробник 1-2023/8 (1241).py
@@ -2,8 +2,8 @@ from itertools import product
 
 
 count = 0
-for word in product("Р*СЛ*Н", repeat=5):
-    if word.count('*') == 1:
+for word in product("РУСЛАН", repeat=5):
+    if word.count('У') <= 1 and word.count('А') <= 1:
         print(word)
         count += 1
 print("Ответ:", count)
